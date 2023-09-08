58a9d6e9ea284750b2aeada7219e7816eb16cc61
Charibdys <87929585+Charibdys@users.noreply.github.com>
2023-09-08 19:39:47 -0400

    Implement start command

--- src/private-parlor-xt/handlers/command_handlers/stop_command.cr
+++ src/private-parlor-xt/handlers/command_handlers/stop_command.cr
@@ -8,7 +8,25 @@ module PrivateParlorXT
     end
 
     def do(ctx : Tourmaline::Context, relay : Relay, access : AuthorizedRanks, database : Database, history : History, locale : Locale)
-      raise NotImplementedError.new("StopCommand has not been implemented yet")
+      return unless (message = ctx.message) && (info = message.from)
+
+      unless (user = database.get_user(info.id.to_i64)) && !user.left?
+        return relay.send_to_user(nil, info.id.to_i64, locale.replies.not_in_chat)
+      end
+
+      user.update_names(info.username, info.full_name)
+      user.set_active
+      user.set_left
+      database.update_user(user)
+
+      relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.left)
+
+      log = Format.substitute_message(locale.logs.left, locale, {
+        "id" => user.id.to_s, 
+        "name" => user.get_formatted_name
+      })
+
+      relay.log_output(log)
     end
   end
 end
