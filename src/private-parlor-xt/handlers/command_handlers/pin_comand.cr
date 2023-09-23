aabd69d5e4ecff5fd5adb83444feed25a78ac4dc
Charibdys <87929585+Charibdys@users.noreply.github.com>
2023-09-22 23:02:01 -0400

    Update pin and unpin commands

--- src/private-parlor-xt/handlers/command_handlers/pin_comand.cr
+++ src/private-parlor-xt/handlers/command_handlers/pin_comand.cr
@@ -7,33 +7,31 @@ module PrivateParlorXT
     def initialize(config : Config)
     end
 
-    def do(ctx : Tourmaline::Context, relay : Relay, access : AuthorizedRanks, database : Database, history : History, locale : Locale)
-      message, user = get_message_and_user(ctx, database, relay, locale)
+    def do(context : Tourmaline::Context, services : Services) : Nil
+      message, user = get_message_and_user(context, services)
       return unless message && user
 
-      unless access.authorized?(user.rank, :Pin)
-        return relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.fail)
-      end
-      unless reply = message.reply_to_message
-        return relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.no_reply)
-      end
-      unless history.get_sender(reply.message_id.to_i64)
-        return relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.not_in_cache)
+      return unless is_authorized?(user, message, :Pin, services)
+ 
+      return unless reply = get_reply_message(user, message, services)
+
+      unless services.history.get_sender(reply.message_id.to_i64)
+        return services.relay.send_to_user(message.message_id.to_i64, user.id, services.locale.replies.not_in_cache)
       end
 
-      user.set_active
-      database.update_user(user)
+      update_user_activity(user, services)
 
-      history.get_all_receivers(reply.message_id.to_i64).each do |receiver, receiver_message|
-        relay.pin_message(receiver, receiver_message)
+      services.history.get_all_receivers(reply.message_id.to_i64).each do |receiver, receiver_message|
+        services.relay.pin_message(receiver, receiver_message)
       end
 
-      log = Format.substitute_message(locale.logs.pinned, {
+      log = Format.substitute_message(services.locale.logs.pinned, {
         "id"   => user.id.to_s,
         "name" => user.get_formatted_name,
         "msid" => reply.message_id.to_s,
       })
-      relay.log_output(log)
+
+      services.relay.log_output(log)
 
       # On success, a Telegram system message
       # will be displayed saying that the bot has pinned the message
--- src/private-parlor-xt/handlers/command_handlers/unpin_command.cr
+++ src/private-parlor-xt/handlers/command_handlers/unpin_command.cr
@@ -5,48 +5,44 @@ module PrivateParlorXT
   @[RespondsTo(command: "unpin", config: "enable_unpin")]
   class UnpinCommand < CommandHandler
     def initialize(config : Config)
-      @blacklist_contact = config.blacklist_contact
     end
 
-    def do(ctx : Tourmaline::Context, relay : Relay, access : AuthorizedRanks, database : Database, history : History, locale : Locale)
-      message, user = get_message_and_user(ctx, database, relay, locale)
+    def do(context : Tourmaline::Context, services : Services) : Nil
+      message, user = get_message_and_user(context, services)
       return unless message && user
 
-      unless access.authorized?(user.rank, :Unpin)
-        return relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.fail)
-      end
+      return unless is_authorized?(user, message, :Unpin, services)
 
       if reply = message.reply_to_message
-        unless history.get_sender(reply.message_id.to_i64)
-          return relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.not_in_cache)
+        unless services.history.get_sender(reply.message_id.to_i64)
+          return services.relay.send_to_user(message.message_id.to_i64, user.id, services.locale.replies.not_in_cache)
         end
 
-        history.get_all_receivers(reply.message_id.to_i64).each do |receiver, receiver_message|
-          relay.unpin_message(receiver, receiver_message)
+        services.history.get_all_receivers(reply.message_id.to_i64).each do |receiver, receiver_message|
+          services.relay.unpin_message(receiver, receiver_message)
         end
 
-        log = Format.substitute_message(locale.logs.unpinned, {
+        log = Format.substitute_message(services.locale.logs.unpinned, {
           "id"   => user.id.to_s,
           "name" => user.get_formatted_name,
           "msid" => reply.message_id.to_s,
         })
       else
-        database.get_active_users.each do |receiver|
-          relay.unpin_latest_pin(receiver)
+        services.database.get_active_users.each do |receiver|
+          services.relay.unpin_latest_pin(receiver)
         end
 
-        log = Format.substitute_message(locale.logs.unpinned_recent, {
+        log = Format.substitute_message(services.locale.logs.unpinned_recent, {
           "id"   => user.id.to_s,
           "name" => user.get_formatted_name,
         })
       end
 
-      user.set_active
-      database.update_user(user)
+      update_user_activity(user, services)
 
-      relay.log_output(log)
+      services.relay.log_output(log)
 
-      relay.send_to_user(message.message_id.to_i64, user.id, locale.replies.success)
+      services.relay.send_to_user(message.message_id.to_i64, user.id, services.locale.replies.success)
     end
   end
 end
